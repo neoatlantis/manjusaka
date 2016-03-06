530d23b65a7056b6a71f9ea0444fd7bfc2e7638c
neoatlantis <rijndael@yeah.net>
2016-03-06 04:15:33 +0100

    coding on input of category password

---
+++ jssrc/decryptor.js
@@ -0,0 +1,11 @@
+var crypto = require('./crypto.js');
+
+
+
+module.exports.testCategoryPassword = function(password){
+    return true;
+}
+
+module.exports.countCategoryPasswords = function(){
+    return 1;
+}
--- jssrc/ui-input-category-password.js
+++ jssrc/ui-input-category-password.js
@@ -1,5 +1,29 @@
-var $ = require('jquery');
+var $ = require('jquery'),
+    decryptor = require('./decryptor.js');
+
+var selBase = '#input-category-password';
+
+$(selBase + ' input').on('change focusout', function(){
+    var val = $(this).val().trim();
+    if(!val) return;
+    if(decryptor.testCategoryPassword(val)){
+        var ackCount = decryptor.countCategoryPasswords();
+        $(this).val('');
+        $(selBase + ' [name="ack-count"]').text(ackCount);
+        if(ackCount > 0) $(selBase + ' button').attr('disabled', false);
+    }
+});
+
+$(selBase + ' button').click(function(){
+    require('./ui-answer-questions.js').show();
+    module.exports.hide();
+});
+
+
+module.exports.hide = function(){
+    $(selBase).hide();
+}
 
 module.exports.show = function(){
-    $('#input-category-password').show();
+    $(selBase).show();
 }
